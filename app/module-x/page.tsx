9981511c36d77ca3a7279af508b0eb1e06237105
refined-x <tower1229@foxmail.com>
2024-02-23 15:17:45 +0800

    chore: ad page

--- app/module-x/page.tsx
+++ app/module-x/page.tsx
@@ -1,9 +1,5 @@
 'use client';
 
 export default function HomePage() {
-  return (
-    <div className="text-center">
-      page x
-    </div>
-  );
+  return <div className='text-center'>page x</div>;
 }
